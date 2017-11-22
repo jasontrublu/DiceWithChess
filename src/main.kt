644ae176d162713c96df61426b5b4e555c0cf5ee
Jason Peper <trublu@c-peper.de>
2017-11-22 21:01:32 +0000

    extract function, add onclick event listener

--- src/main.kt
+++ src/main.kt
@@ -7,21 +7,27 @@ fun main(args: Array<String>) {
     println("Hello JavaScript!")
     val board = document.getElementById("board") as HTMLTableElement
     val cells = CellContainer()
+    createCellsOnBoard(board, cells)
+    (document.getElementById("field33") as HTMLTableCellElement).innerHTML = "6"
+}
+
+private fun createCellsOnBoard(board: HTMLTableElement, cells: CellContainer) {
     for (i in 1..9) {
         val row = board.insertRow()
         row.id = "row$i"
         for (j in 1..9) {
             val cell = row.insertCell() as HTMLTableCellElement
-            cells[i, j] =cell
+            cells[i, j] = cell
+            cell.onclick = {
+                cell.innerHTML = "6"
+                it
+            }
             cell.id = "field$i$j"
             cell.addClass("field")
         }
     }
-    (document.getElementById("field33") as HTMLTableCellElement).innerHTML = "6"
 }
 
-inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int) -> INNER): Array<Array<INNER>>
-        = Array(sizeOuter) { Array(sizeInner, innerInit) }
 
 class CellContainer {
     val cells = array2d<HTMLTableCellElement?>(9, 9) { null }
@@ -30,4 +36,6 @@ class CellContainer {
         cells[i - 1][j - 1] = cell
     }
 
+    inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int) -> INNER): Array<Array<INNER>>
+            = Array(sizeOuter) { Array(sizeInner, innerInit) }
 }
